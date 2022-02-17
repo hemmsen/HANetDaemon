82ffa2f9eec99fb5fe4b3a062942954896161cc1
Mathias Hemmsen <mat@trifork.com>
2022-02-17 23:34:14 +0100

    added new carbon dioxide strategies

---
+++ Infrastructure/Strategies/CarbonDioxide/CarbonDioxideGreenLevelLivingroomStrategy .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemmsenHA.Infrastructure.Strategies.CarbonDioxide
+{
+    public class CarbonDioxideGreenLevelLivingroomStrategy : ICarbonDioxideChangedStrategy
+    {
+        private Entities entities;
+        private IServices services;
+        public CarbonDioxideGreenLevelLivingroomStrategy(IHaContext haContext, IScheduler scheduler, IMediator mediator)
+        {
+            entities = new Entities(haContext);
+            this.services = new Services(haContext);
+        }
+
+        public bool CanHandle(CarbonDioxideChanged carbonDioxideChanged)
+        {
+            return carbonDioxideChanged.EntityId == entities.Sensor.NetatmoEngelstoft157IndoorCo2.EntityId && carbonDioxideChanged.NewCarbonDioxide.State <= 1000;
+        }
+
+        public Task DoAction(CarbonDioxideChanged carbonDioxideChanged)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Infrastructure/Strategies/CarbonDioxide/CarbonDioxideRedLevelLivingroomStrategy.cs
@@ -0,0 +1,36 @@
+﻿namespace HemmsenHA.Infrastructure.Strategies.CarbonDioxide
+{
+    public class CarbonDioxideRedLevelLivingroomStrategy : ICarbonDioxideChangedStrategy
+    {
+        private Entities entities;
+        private IServices services;
+        private readonly IMediator mediator;
+        public CarbonDioxideRedLevelLivingroomStrategy(IHaContext haContext, IScheduler scheduler, IMediator mediator)
+        {
+            entities = new Entities(haContext);
+            this.services = new Services(haContext);
+            this.mediator = mediator;
+        }
+
+        public bool CanHandle(CarbonDioxideChanged carbonDioxideChanged)
+        {
+            return carbonDioxideChanged.EntityId == entities.Sensor.NetatmoEngelstoft157IndoorCo2.EntityId && carbonDioxideChanged.NewCarbonDioxide.State >= 2250;
+        }
+
+        public async Task DoAction(CarbonDioxideChanged carbonDioxideChanged)
+        {
+            services.Light.TurnOn(ServiceTarget.FromEntity(entities.Light.LivingroomLights.EntityId), new LightTurnOnParameters() { Flash = "short" });
+            
+            //build notification
+            var notificationMessage = new SpeakerNotification()
+            {
+                EntityId = entities.MediaPlayer.TvStue.EntityId,
+                NotificationMessage = $"Kultioxid niveau i {entities.MediaPlayer.TvStue.Area} er nu på {carbonDioxideChanged.NewCarbonDioxide.State} ppm. Luft ud med det samme!"
+            };
+            //Send speaker notification
+            await mediator.Publish(notificationMessage);
+
+            services.Light.TurnOn(ServiceTarget.FromEntity(entities.Light.LivingroomLights.EntityId), new LightTurnOnParameters() { Flash = "short" });
+        }
+    }
+}
