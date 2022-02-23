3ff415debd4dd8b2f1c548446f4a65d32c683a6e
hemmsen <mathias@vejensten.dk>
2022-02-23 15:14:07 +0100

    Feature/move (#7)
    * added move app
    
    Co-authored-by: Mathias Hemmsen <mat@trifork.com>

---
+++ HemmsenHA/Core/NotificationMessages/LowPowerOnDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemmsenHA.Core.NotificationMessages
+{
+    public class LowPowerOnDevice : BaseEntityStateChanged, INotification
+    {
+        public double? PowerLevel { get; set; }
+    }
+}
