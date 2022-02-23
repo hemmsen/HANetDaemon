b21f4e7f33ddc77840a0a335e6eacca079ee6912
Mathias Hemmsen <mat@trifork.com>
2022-02-23 15:12:54 +0100

    added move app

---
+++ HemmsenHA/apps/Sonos/SonosMoveApp.cs
@@ -0,0 +1,25 @@
+﻿namespace HemmsenHA.apps.Sonos
+{
+    [NetDaemonApp]
+    public class SonosMoveApp
+    {
+        public SonosMoveApp(IHaContext haContext, IMediator mediator)
+        {
+            var entities = new Entities(haContext);
+
+            entities.Sensor.MoveBattery
+                .StateAllChanges()
+                .Where(state => state?.New?.State < 20)
+                .Subscribe(state =>
+                {
+                    var lowBatteryAlert = new LowPowerOnDevice()
+                    {
+                        EntityId = state.Entity.EntityId,
+                        PowerLevel = state?.New?.State
+                    };
+
+                    mediator.Publish(lowBatteryAlert);
+                });
+        }
+    }
+}
