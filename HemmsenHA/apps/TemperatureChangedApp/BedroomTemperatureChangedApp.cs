b380d033bcf68d4193b3c2d6af0d9ffbe62842e5
Mathias Hemmsen <mat@trifork.com>
2022-03-06 11:36:54 +0100

    minor bugfix plus extra logging

--- HemmsenHA/apps/TemperatureChangedApp/BedroomTemperatureChangedApp.cs
+++ HemmsenHA/apps/TemperatureChangedApp/BedroomTemperatureChangedApp.cs
@@ -3,14 +3,15 @@
     [NetDaemonApp]
     public class BedroomTemperatureChangedApp
     {
-        public BedroomTemperatureChangedApp(IHaContext haContext, IMediator mediator)
+        public BedroomTemperatureChangedApp(IHaContext haContext, IMediator mediator, ILogger<BedroomTemperatureChangedApp> logger)
         {
             var entities = new Entities(haContext);
 
             entities.Climate.BedroomThermostatThermostat
                 .StateAllChanges()
-                .Subscribe(entitystate =>
+                .Subscribe(async entitystate =>
                 {
+                    logger.LogInformation($"Climate state changed - Old current tempearture: {entitystate?.Old?.Attributes?.CurrentTemperature} and new current temperature: {entitystate?.New?.Attributes?.CurrentTemperature}");
                     var tempChanged = new ClimateChangedNotification()
                     {
                         EntityId = entitystate.Entity.EntityId,
