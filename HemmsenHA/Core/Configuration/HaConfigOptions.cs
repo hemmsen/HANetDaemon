67f6afe44bf6beda6eb5e12b202efeccf7734bbd
Mathias Hemmsen <mat@trifork.com>
2022-03-08 19:20:47 +0100

    addeed new configurations

--- HemmsenHA/Core/Configuration/HaConfigOptions.cs
+++ HemmsenHA/Core/Configuration/HaConfigOptions.cs
@@ -2,13 +2,15 @@
 {
     public class HaConfigOptions
     {
-        public double BedroomTemp{ get; set; }
-        public double CO2YellowLow{ get; set; }
+        public double BedroomTemp { get; set; }
+        public double CO2GreenHigh { get; set; }
         public double CO2YellowHigh { get; set; }
+        public double CO2RedHigh { get; set; }
         public double HighTempAlarmBedroom { get; set; }
         public double LowTempAlarmEva { get; set; }
         public double LowTempAlarmBedroom { get; set; }
         public double EspressoReadyPowerConsumption { get; set; }
+        public double MinutesToStartEspressoMachineBeforeAlarm { get; set; }
         public TimeSpan MuteTemperatureNotificationsAfter { get; set; }
     }
 }
